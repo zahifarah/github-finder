4ee7af3eca46f96dbd39ecfd51e78556d5c87a3c
Zahi Farah <zahi.farah@protonmail.com>
2021-08-26 10:10:57 +0300

    Define two methods.: one with an API call to Github via searchUsers() and another to clear the search result via clearUsers()

--- src/App.js
+++ src/App.js
@@ -11,21 +11,32 @@ class App extends Component {
       loading: false,
    };
 
-   async componentDidMount() {
+   // Search Github users
+   searchUsers = async (text) => {
       this.setState({ loading: true });
       const res = await axios.get(
-         `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_TOKEN}`
+         `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_TOKEN}`
       );
-      this.setState({ users: res.data, loading: false });
-   }
+      this.setState({ users: res.data.items, loading: false });
+   };
+
+   // Clear users from state
+   clearUsers = () => this.setState({ users: [], loading: false });
 
    render() {
+      const { users, loading } = this.state;
       return (
          <div className="App">
             <Navbar />
             <div className="container">
-               <Search />
-               <Users loading={this.state.loading} users={this.state.users} />
+               {/* assign "this.searchUsers" return value (text in this case) 
+               to be passed in to searchUsers method in App.js */}
+               <Search
+                  searchUsers={this.searchUsers}
+                  clearUsers={this.clearUsers}
+                  showClear={users.length > 0 ? true : false}
+               />
+               <Users loading={loading} users={users} />
             </div>
          </div>
       );
