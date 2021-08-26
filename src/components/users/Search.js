4a82eed2b0017089a2ced03a87baa49e7332a040
Zahi Farah <zahi.farah@protonmail.com>
2021-08-26 10:14:09 +0300

    Import and add type-checking via prop-types. Define prop function searchUsers(), fires on form submit. Implement logic to show or hide Clear search button.

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Search extends Component {
    state = {
       text: "",
+   };
+
+   static propTypes = {
+      searchUsers: PropTypes.func.isRequired,
+      clearUsers: PropTypes.func.isRequired,
+      showClear: PropTypes.bool.isRequired,
    };
 
    // "e.target.name" in brackets to pass it as a key, returns "text" in this case
@@ -15,10 +22,13 @@ class Search extends Component {
 
    onSubmit(e) {
       e.preventDefault();
-      console.log(this.state.text);
+      // create props function called searchUsers(), passing in the text entered in search bar.
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
    }
 
    render() {
+      const { showClear, clearUsers } = this.props;
       return (
          <div>
             <form onSubmit={this.onSubmit.bind(this)} className="form">
@@ -35,6 +45,11 @@ class Search extends Component {
                   className="btn btn-dark btn-block"
                />
             </form>
+            {showClear && (
+               <button className="btn btn-light btn-block" onClick={clearUsers}>
+                  Clear
+               </button>
+            )}
          </div>
       );
    }
