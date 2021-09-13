695098fe545c70489dc72e3283a621cd00c627a0
Zahi Farah <zahi.farah@protonmail.com>
2021-09-13 15:48:27 +0300

    Fix routes.

--- src/App.js
+++ src/App.js
@@ -9,95 +9,95 @@ import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
 
+const tkn = process.env.REACT_APP_GITHUB_TOKEN;
+
 class App extends Component {
-   state = {
-      users: [],
-      user: {},
+  state = {
+    users: [],
+    user: {},
+    loading: false,
+    alert: null,
+  };
+
+  // Search Github users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${tkn}`
+    );
+    this.setState({
+      users: res.data.items,
       loading: false,
-      alert: null,
-   };
+    });
+  };
 
-   // Search Github users
-   searchUsers = async (text) => {
-      this.setState({ loading: true });
-      const res = await axios.get(
-         `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_TOKEN}`
-      );
-      this.setState({
-         users: res.data.items,
-         loading: false,
-      });
-   };
+  // Get Single Github user
+  getUser = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?${username}?client_id=${process.env.REACT_APP_GITHUB_TOKEN}`
+    );
+    this.setState({
+      user: res.data,
+      loading: false,
+    });
+  };
 
-   // Get Single Github user
-   getUser = async (username) => {
-      this.setState({ loading: true });
-      const res = await axios.get(
-         `https://api.github.com/users?${username}?client_id=${process.env.REACT_APP_GITHUB_TOKEN}`
-      );
-      this.setState({
-         user: res.data,
-         loading: false,
-      });
-   };
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
 
-   // Clear users from state
-   clearUsers = () => this.setState({ users: [], loading: false });
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
 
-   // Set Alert
-   setAlert = (msg, type) => {
-      this.setState({ alert: { msg: msg, type: type } });
-      setTimeout(() => this.setState({ alert: null }), 5000);
-   };
-
-   render() {
-      //   const { users, user, loading } = this.state;
-      return (
-         <BrowserRouter>
-            <div className="App">
-               <Navbar />
-               <div className="container">
-                  <Alert alert={this.state.alert} />
-                  <Switch>
-                     <Route
-                        exact
-                        path="/"
-                        render={(props) => (
-                           <Fragment>
-                              <Search
-                                 searchUsers={this.searchUsers}
-                                 clearUsers={this.clearUsers}
-                                 showClear={
-                                    this.state.users.length > 0 ? true : false
-                                 }
-                                 setAlert={this.setAlert}
-                              />
-                              <Users
-                                 loading={this.state.loading}
-                                 users={this.state.users}
-                              />
-                           </Fragment>
-                        )}
-                     />
-                     <Route exact path="/about" component={About} />
-                     <Route
-                        exact
-                        path="/user/:username"
-                        render={(props) => (
-                           <User
-                              {...props} // any extra props we have
-                              getUser={this.getUser}
-                              user={this.state.user} // send state of user to <User/> component
-                              loading={this.state.loading}
-                           />
-                        )}
-                     />
-                  </Switch>
-               </div>
-            </div>
-         </BrowserRouter>
-      );
-   }
+  render() {
+    //   const { users, user, loading } = this.state;
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/home"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={this.state.users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:username"
+                render={props => (
+                  <User
+                    {...props} // any extra props we have
+                    getUser={this.getUser}
+                    user={this.state.user} // send state of user to <User/> component
+                    loading={this.state.loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,33 +3,33 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const Navbar = ({ icon, title }) => {
-   return (
-      <nav className="navbar bg-primary">
-         <h1>
-            <i className={icon} /> {title}
-         </h1>
-         <ul>
-            <li>
-               <Link to="/">Home</Link>
-            </li>
-            <li>
-               <Link to="/about">About</Link>
-            </li>
-         </ul>
-      </nav>
-   );
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 // defaults to this
 Navbar.defaultProps = {
-   title: "Github Finder",
-   icon: "fab fa-github",
+  title: "Github Finder",
+  icon: "fab fa-github",
 };
 
 // type-checking
 Navbar.propTypes = {
-   title: PropTypes.string.isRequired,
-   icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
 };
 
 export default Navbar;
